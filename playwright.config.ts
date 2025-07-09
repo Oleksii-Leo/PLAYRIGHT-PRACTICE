003c02a64a1649b9666bc0e05142e5e1bcae0a81
Oleksii-Leo <al.provita@gmail.com>
2025-07-09 15:32:17 +0300

    29

--- playwright.config.ts
+++ playwright.config.ts
@@ -54,6 +54,13 @@ export default defineConfig({
       name: "smoke",
       use: { ...devices["Desktop Chrome"] },
       testIgnore: "*/setup/**.ts",
+      testMatch: "*/e2e/**.ts",
+      dependencies: ["setup"],
+    },
+    {
+      name: "api",
+      testIgnore: "*/setup/**.ts",
+      testMatch: "*/api/**.ts",
       dependencies: ["setup"],
     },
 
--- test-data/users.ts
+++ test-data/users.ts
@@ -1,5 +1,7 @@
 export const usersList = {
   mainUser: {
+    name: "TestName",
+    lastName: "TestLastName",
     email: "johnywalker2@gmail.com",
     password: "Qwerty123",
   },
